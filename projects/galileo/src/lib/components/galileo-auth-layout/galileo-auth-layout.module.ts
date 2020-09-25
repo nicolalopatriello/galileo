8d6952897fcd57d61cee19f199035efee0817c9b
Nicola Lopatriello <nicola.lopatriello@gfmintegration.it>
2020-09-25 23:30:35 +0200

    ci: test ci

--- projects/galileo/src/lib/components/galileo-auth-layout/galileo-auth-layout.module.ts
+++ projects/galileo/src/lib/components/galileo-auth-layout/galileo-auth-layout.module.ts
@@ -5,6 +5,7 @@ import {RouterModule} from '@angular/router';
 import {GalileoCommonModule} from '../../utils/galileo-common/galileo-common.module';
 
 
+
 @NgModule({
   declarations: [AuthLayoutComponent],
   imports: [
