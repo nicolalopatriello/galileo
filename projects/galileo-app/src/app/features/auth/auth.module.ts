4c009e7edb6ee3c3b543efd1ff5401ba4bd69544
Nicola Lopatriello <nicola.lopatriello@gfmintegration.it>
2020-09-28 17:31:54 +0200

    ci: test

--- projects/galileo-app/src/app/features/auth/auth.module.ts
+++ projects/galileo-app/src/app/features/auth/auth.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  GalileoAuthLayoutModule,
-  GalileoForgotModule,
-  GalileoSignInModule,
-  GalileoSignUpModule
-} from '@nicolalopatriello/galileo';
 import {RouterModule} from '@angular/router';
 import {AuthSignInComponent} from './containers/auth-sign-in.component';
 import {AuthSignUpComponent} from './containers/auth-sign-up.component';
 import {AuthForgotPasswordComponent} from './containers/auth-forgot-password.component';
 import {AuthComponent} from './auth.component';
+import {GalileoSignInModule} from '../../../../../galileo/src/lib/components/galileo-sign-in/galileo-sign-in.module';
+import {GalileoSignUpModule} from '../../../../../galileo/src/lib/components/galileo-sign-up/galileo-sign-up.module';
+import {GalileoForgotModule} from '../../../../../galileo/src/lib/components/galileo-forgot/galileo-forgot.module';
+import {GalileoAuthLayoutModule} from '../../../../../galileo/src/lib/components/galileo-auth-layout/galileo-auth-layout.module';
 
 
 
