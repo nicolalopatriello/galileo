a616deea275eb32b4cbafd9f582635a94f0bf8c8
Nicola Lopatriello <nicola.lopatriello@gfmintegration.it>
2021-01-05 22:29:48 +0100

    feat(gll-sidebar): svgPath as sidebar icons

--- projects/galileo/src/lib/components/galileo-sidebar/sidebar/sidebar.component.ts
+++ projects/galileo/src/lib/components/galileo-sidebar/sidebar/sidebar.component.ts
@@ -37,9 +37,11 @@ import {Utils} from '../../../utils/utils';
                 (sidebarItemsGroups.get(groupLabel).groupLabel.show | async) ? '5px solid ' + (sidebarItemsGroups.get(groupLabel).groupLabel.activeItemGroupBorderColor | async) : null}"
                 [ngClass]="{'sidenav--closed': !sideBarOpened}">
                 <fa-icon
+                  *ngIf="item.faIcon"
                   [icon]="['fas', item.faIcon]"
                   class="fa-1x" [ngClass]="{'item-icon': !rla.isActive,
                               'item-icon-active': rla.isActive}"></fa-icon>
+                <img *ngIf="item.svgPath" [src]="item.svgPath" style="width: 20px">
                 <div [ngClass]="{'pl-2': sideBarOpened}" class="d-flex justify-content-center"
                      *ngIf="sideBarOpened">
                   <div class="d-flex align-self-center sidenav__label"
